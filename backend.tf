13904da0f7c3f0e53c43d12e72f02efacc70cb1c
gsmothilal <mothilal488@gmail.com>
2025-07-31 13:41:55 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-of-carbon"              
-    storage_account_name = "storage88998898"           
+    storage_account_name = "azureblobstorage64"           
     container_name       = "container5579"            
     key                  = "terraform.tfstate"
   }
